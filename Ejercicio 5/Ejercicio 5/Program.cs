48142fca6b14fdb7aac02c925f94267e82f107f1
Jose Luis <47016453+JLRG806@users.noreply.github.com>
2021-02-22 09:36:31 -0600

    Ejercicio 5

---
+++ Ejercicio 5/Ejercicio 5/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Ejercicio_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.BackgroundColor = ConsoleColor.Red;
+            Console.Clear();
+            //Declaración de variables a usar.
+            
+            int length = 30;
+            int l = 1;
+
+            for (int z = 0; z < l; z++)
+            {
+                try
+                {
+                    Console.WriteLine("\nImpresion de numeros del 1 al 30.");
+                    
+
+                    for (int i = 1; i <= length; i++)
+                    {
+                        if (i != 25)
+                        {
+                            Console.WriteLine("\nNumero {0}.", i);
+                        }
+                        else
+                        {
+                            Console.WriteLine("\nNumero no permitido!!!!!!!");
+                        }
+
+                    }               
+
+                }
+                catch (Exception)
+                {
+                    l++;
+                    Console.WriteLine($" \n\nPor favor ingrese un numero sin decimales");
+
+                }
+
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
